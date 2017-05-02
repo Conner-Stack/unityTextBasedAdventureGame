7ec7b7786680210ae0f1638e4a1beaeaabd01202
Conner-Stack <connerstack@gmail.com>
2017-05-02 11:12:54 -0500

    First Push
    First Push for the unity TextBasedAdventure recode

---
+++ Assets/Scripts/GameBehavior.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+public class GameBehavior : MonoBehaviour {
+    List<GameObject>[,] Boxes;
+    [SerializeField]
+    public GameObject boxPrefab;
+    private int width = 10;
+    private int height = 10;
+    public Rigidbody rgd;
+   
+    void CreateGrid()
+    {
+        
+        for (int x = 0; x < width; x++)
+        {
+            for (int y = 0; y < height; y++)
+            {
+            Boxes[x,y] = Instantiate<>   
+            }
+             = Instantiate(boxPrefab) as GameObject;
+        }
+    }
+    // Use this for initialization
+    void Start () {
+        Boxes = new List<GameObject>();
+        Getcompo
+    }
+	// Update is called once per frame
+	void Update () {
+         Inst
+}
---
+++ Assets/Scripts/StartScreenBehavior.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class StartScreenBehavior : MonoBehaviour {
+    [SerializeField]
+    // Use this for initialization
+    public GameObject StartScreen;
+    void Awake()
+    {
+
+    }
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
